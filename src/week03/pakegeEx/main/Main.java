73d462813724a0aceda04c8fa1c7c1f627359e76
chungwonJ <wjdcjddnjs1234@gmail.com>
2025-01-08 17:26:58 +0900

    3주차

---
+++ src/week03/pakegeEx/main/Main.java
@@ -0,0 +1,10 @@
+package week03.pakegeEx.main;
+
+import week03.pakegeEx.pk1.Car;
+
+public class Main {
+    public static void main(String[] args) {
+        Car car = new Car();
+        car.horn();
+    }
+}
